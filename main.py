3776ab8a229852a1843eb3df4f7946874e8f4ccb
wilodietch <wilodietch@gmail.com>
2020-09-11 23:38:30 +0000

    new strings

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ print("Welcome to Madlibs")
 food = input ("Enter a food: ")
 color = input ("Enter a color: ")
 location = input ("Name a location: ")
+number = input ("Enter a number: ")
 
-print(f"For lunch I had a {color} {food} in {location}.")
+print(f"For lunch I had {number} {color} {food} in {location}. If I ever feel {color}, all I have to dream about is driving in {location}. ")
 
