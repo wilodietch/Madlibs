801ef72225980ec90b417488c4490497c7407a0d
wilodietch <wilodietch@gmail.com>
2020-09-11 23:41:29 +0000

    text strings and date

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ color = input ("Enter a color: ")
 location = input ("Name a location: ")
 number = input ("Enter a number: ")
 
-print(f"For lunch I had {number} {color} {food} in {location}. If I ever feel {color}, all I have to dream about is driving in {location}. ")
+print(f"For lunch I had {number} {color} {food}s in {location}. If I ever feel {color}, all I have to dream about is driving in {location} on Sept {number}, 2020. ")
 
